79c5507e8c98ea9111930353dab373185f0ca6af
Iskandirov <Iskandirov@A33COMP13>
2019-03-26 19:14:08 +0200

    Another one time .i do it

--- WebApi/Factory.cs
+++ WebApi/Factory.cs
@@ -15,7 +15,7 @@ namespace WebApi
         public Factory()
             : base("name=Factory")
         {
-            Database.SetInitializer<Factory>(new CustomInit<Factory>())
+            Database.SetInitializer<Factory>(new CustomInit<Factory>());
         }
         /// <summary>
         /// table for Workers
